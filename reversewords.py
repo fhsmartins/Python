65dab90c959a75e0617221691a4e6da5893abb28
Fabio Martins <fhsm13@gmail.com>
2021-06-02 11:06:43 -0300

    Update reversewords.py

--- reversewords.py
+++ reversewords.py
@@ -2,20 +2,11 @@
 Author: fhsmartins
 """
 
-def reverse_words_and_swap_cases(sentence: str) -> str:
-    reverse_words_with_swap_cases = list()
-    for word in sentence.split()[: : -1]:
-        wrd = list()
-        for character in word:
-            if character.islower():
-                wrd.append(character.upper())
-                continue
-            wrd.append(character.lower())
-        reverse_words_with_swap_cases.append(''.join(wrd))
-    return ' '.join(reverse_words_with_swap_cases)
+def reverse_words_order_and_swap_cases(sentence):
+     words = sentence.split()
 
-def main():
-    print(reverse_words_and_swap_cases(input().strip()))
+     words = list(reversed(words))
+    
+     j = " ".join(words)
 
-if __name__ == "__main__":
-    main()
+     return j.swapcase()
